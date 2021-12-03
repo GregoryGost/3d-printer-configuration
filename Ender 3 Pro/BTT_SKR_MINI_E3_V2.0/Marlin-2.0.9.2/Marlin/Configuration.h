8afeacf45632b3ab87dbfae4f2d19dbb8b41fa7f
GregoryGost <huzver@gmail.com>
2021-12-03 22:13:08 +0300

    Fix X offset

--- Ender 3 Pro/BTT_SKR_MINI_E3_V2.0/Marlin-2.0.9.2/Marlin/Configuration.h
+++ Ender 3 Pro/BTT_SKR_MINI_E3_V2.0/Marlin-2.0.9.2/Marlin/Configuration.h
@@ -1380,7 +1380,7 @@
 #define Y_BED_SIZE 235
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
-#define X_MIN_POS 0
+#define X_MIN_POS -10 // For E3BMGS - Крепление директ BMG(clone) + E3DV6 экструдера
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
